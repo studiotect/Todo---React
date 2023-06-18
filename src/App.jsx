098af7e48e8c5ba28fff0c0ce1bc7f26b5ad08f3
Jason Triemert <jaytriemert@gmail.com>
2023-06-17 22:19:09 -0500

    added app.jsx, it is mostly setup, but will need to split components out

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,38 @@ import "./style.css"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
-  function handleSubmit(e) {
-    e.preventDefault()
-
+  const [todos, setTodos] = useState([])
+  
+  function toggleTodo(id, completed) {
     setTodos(currentTodos => {
-      return [
-        ...currentTodos,
-        {
-          id: task.randomUUID(), title: newItem, completed:
-            false
-        },
-      ]
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== todo.id)
     })
   }
   return (
     <>
-      <form onSubmit className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
-        </div>
-        <button className="btn">Add</button>
-      </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return (
-            <li>
+            <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input type="checkbox" checked={todo.completed}
+                  onChange={e => toggleTodo(todo.id, e.target.checked)} />
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger">Delete</button>
             </li>
           )
         })}
@@ -44,3 +44,4 @@ export default function App() {
     </>
   )
 }
+// console.log(todo.completed);
---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,29 @@
+export function NewTodoForm() {
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(), title: newItem, completed:
+            false
+        },
+      ]
+    })
+    setNewItem("")
+  }
+  return
+  (
+
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input value={newItem} onChange={e => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  )
